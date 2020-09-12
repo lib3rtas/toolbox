42883c1fbfea57ed4ee7d6465086f999fad70961
Florian <florian.hammer.1996@web.de>
2020-09-12 13:41:30 +0200

    Update connect_fhnw_data.sh
    made save folder generic

--- bash/connect_fhnw_data.sh
+++ bash/connect_fhnw_data.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo mount -t cifs -o user=vorname.nachname,domain=edu,vers=3.0,sec=ntlmssp //fsemu18.edu.ds.fhnw.ch/e_18_data11$/ /home/fhammer/FHNW/
+sudo mount -t cifs -o user=vorname.nachname,domain=edu,vers=3.0,sec=ntlmssp //fsemu18.edu.ds.fhnw.ch/e_18_data11$/ /target/folder
